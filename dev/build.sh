f9164e179b844b961edc73d3d3784a4ea5698c38
Vitaliy Gulyy <vgulyy@redhat.com>
2020-04-09 14:51:23 +0300

    Extract commands to deparate files
    Signed-off-by: Vitaliy Gulyy <vgulyy@redhat.com>

---
+++ dev/build.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+#
+# Copyright (c) 2019 Red Hat, Inc.
+# This program and the accompanying materials are made
+# available under the terms of the Eclipse Public License 2.0
+# which is available at https://www.eclipse.org/legal/epl-2.0/
+#
+# SPDX-License-Identifier: EPL-2.0
+#
+# See: https://sipb.mit.edu/doc/safe-shell/
+
+if [ -f "/tmp/username" ]; then
+    USERNAME=`cat /tmp/username`
+    buildah bud -t quay.io/$USERNAME/che-sidecar-kubernetes-tooling:dev .
+    echo 'Image built'
+    buildah images
+    echo 'Done'
+else
+    echo 'You need to login the first'
+fi
